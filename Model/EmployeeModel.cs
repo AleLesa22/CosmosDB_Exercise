63ae17b0584daba0d3ac892b7a5f8ba0d52c325f
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2023-01-17 15:05:01 +0100

    Initial Commit for the Employees database

---
+++ Model/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Employees_CosmosDB.Model
+{
+    public class EmployeeModel
+    {
+        public string? id { get; set; }
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public string? Department { get; set; }
+        public string? Designation { get; set; }
+        public string? JoiningDate { get; set; }
+    }
+}
